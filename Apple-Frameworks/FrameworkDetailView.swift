c0cf2991b9da4754b356baedb98654d666ceacb6
joaopbueno <mrjoao16@gmail.com>
2024-11-26 19:33:52 -0300

    link web para o safari

--- Apple-Frameworks/FrameworkDetailView.swift
+++ Apple-Frameworks/FrameworkDetailView.swift
@@ -10,6 +10,7 @@ import SwiftUI
 struct FrameworkDetailView: View {
     var framework: Framework
     @Binding var isShowingDetailView: Bool
+    @State private var isShowingSafariView: Bool = false
     
     var body: some View {
         VStack{
@@ -34,11 +35,15 @@ struct FrameworkDetailView: View {
                 .padding()
             Spacer()
             Button{
-                
+                isShowingSafariView = true
             }label: {
                 AFButton(title: "Learn More")
             }
         }
+        .sheet(isPresented: $isShowingSafariView, content: {
+            SafariView(url: (URL(string: framework.urlString) ?? URL(string: "https://www.apple.com/"))!)
+        })
+    
     }
 }
 
